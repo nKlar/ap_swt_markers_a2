0dc48a61d85618b9956ba1e68fb192d7f188a31c
nKlar <maximmixam1@gmail.com>
2017-10-21 14:44:35 +0200

    Undo prev

--- config.cpp
+++ config.cpp
@@ -26,7 +26,7 @@ class Extended_PreInit_EventHandlers
 {
 	class swt_markers
 	{
-		Init="[] spawn { '\swt_markers\fn_init.sqf' }";
+		Init="call compile preProcessFileLineNumbers '\swt_markers\fn_init.sqf'";
 	};
 };
 class CfgMarkers
